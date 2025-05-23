062c48ea97e071b6e48375b9cd54526e0885f76c
Prashanth Naroju <prashanth.naro@gmail.com>
2025-05-23 13:34:15 +0530

    add the node backend boiler plate code

---
+++ app.js
@@ -0,0 +1,17 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Simple route
+app.get('/', (req, res) => {
+  res.send('Hello World from Express!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
